d85468f1c411762b55e38fd9d454db6e5e1fa219
syyePhenomenol <yye3028@gmail.com>
2023-10-14 14:33:15 +0200

    commit message

---
+++ ControllerFixes/Settings/ModMenu.cs
@@ -0,0 +1,57 @@
+namespace ControllerFixes;
+
+public static class ModMenu
+{
+    private static Menu MenuRef;
+    private static GlobalSettings settings => ControllerFixes.settings;
+    private static readonly List<string> Descriptions = new ()
+    {
+        "Normal controller prompts",
+        "Joy-Cons image, Nintendo prompts",
+        "DS4, PS prompts (Prompt for Inventory: Touchpad)",
+        "DS4, PS prompts (Prompt for Inventory: Share button)",
+        "DS4, PS prompts (Prompt for Inventory: Back/Select button)",
+        "Xbox One, XB prompts (Prompt for Inventory: View)",
+        "Xbox 360, XB prompts (Prompt for Inventory: Back/Select)",
+    };
+
+    public static MenuScreen CreateMenuScreen(MenuScreen modListMenu)
+    {
+        MenuRef = new Menu("Controller Fixes", new Element[]
+        {
+            new MenuButton("Link to explanations",
+                "If anything is unclear, please read the steam discussion that this mod is based on",
+                _ => Application.OpenURL("https://steamcommunity.com/sharedfiles/filedetails/?id=2525497719")),
+            
+            Blueprints.HorizontalBoolOption("No Cast",
+                "Remove cast action from focus button/key",
+                b => settings.NoCast = b,
+                () => settings.NoCast),
+
+            new HorizontalOption("Button Skin Type", 
+            Descriptions[settings.ButtonSkinType + 1],
+            new[] {"Default", "1", "2", "3", "4", "5", "6"},
+            (i) =>
+            {
+                settings.ButtonSkinType = i - 1;
+
+                (MenuRef.Find("ButtonPrompt") as HorizontalOption)!.Description = Descriptions[i];
+                MenuRef.Update();
+            },
+            () => settings.ButtonSkinType + 1,
+            Id: "ButtonPrompt"),
+        
+            Blueprints.HorizontalBoolOption("Emulated Xbox Only", 
+            "Forces game to use xbox 360 controller",
+            b => settings.EmulatedXboxOnly = b,
+            () => settings.EmulatedXboxOnly),
+            
+            Blueprints.HorizontalBoolOption("Steam Nintendo Layout",
+                "Use Steam's nintendo controller layout",
+                b => settings.SteamNintendoLayout = b,
+                () => settings.SteamNintendoLayout)
+        });
+
+        return MenuRef.GetMenuScreen(modListMenu);
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# ControllerFixes
+
+This is a newer version of [Button Prompt Switcher](https://github.com/TheMulhima/Button-Prompt-Switcher).
+
+This mod includes:
+- Features carried over from Button Prompt Switcher.
+    - In the Mod menu:
+        - Toggle removing the cast action from the Focus button/key
+        - Toggle the controller skin in Settings -> Controller
+        - Toggle forcing an Xbox controller input
+        - Toggle using the Steam Nintendo controller layout
+    - Fixes detection of certain controllers disconnecting/reconnecting
+    - Fixes detection of certain exotic controllers
+    - For more information, see [tintingaroo's post](https://steamcommunity.com/sharedfiles/filedetails/?id=2525497719) on Steam.
+- Fixing certain controllers (such as Xbox) on Mac not being detected as input.
+    - You must set Settings -> Controller -> Enable Native Input to ON while having this mod installed.
+    - Related discussions on Steam:
+        - [Dash RT isn't working](https://steamcommunity.com/app/367520/discussions/0/1621724915787605440/)
+        - [Controller support on M1 Mac](https://steamcommunity.com/app/367520/discussions/0/3108025660984901686/)
+ 
+If something is not working for you, please make a thread in #modding-help on the [Hollow Knight Discord server](https://discord.com/invite/hollowknight).
